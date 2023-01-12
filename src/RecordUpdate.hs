a4885d0111570308ec1781b38e4c1a06ba167586
Phil de Joux <philderbeast@gmail.com>
2023-01-12 17:30:32 -0500

    Rename incBool to toggleBool.

--- src/RecordUpdate.hs
+++ src/RecordUpdate.hs
@@ -6,6 +6,6 @@ module RecordUpdate where
 import DataFamily (Choice(..))
 import qualified DataFamily as WhoIsB (Fam(FamB), Fam(field))
 
-incBool :: WhoIsB.Fam B -> WhoIsB.Fam B
-incBool b@WhoIsB.FamB{..} =
+toggleBool :: WhoIsB.Fam B -> WhoIsB.Fam B
+toggleBool b@WhoIsB.FamB{..} =
     b{ WhoIsB.field = not field}
